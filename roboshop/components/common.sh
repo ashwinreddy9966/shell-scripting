35d4a54d033aa53be7272b0d3c49d88f52979ffa
manu <manu@local.com>
2022-04-03 21:00:38 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -82,12 +82,12 @@ echo -n "Downloading $COMPONENT component"
 curl -f -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOGFILE}
 stat $?
 
-echo -n "CleanUp Old Content : "
-rm -rf /home/${FUSER}/${COMPONENT} &>>${LOGFILE}
-stat $?
+# echo -n "CleanUp Old Content : "
+# rm -rf /home/${FUSER}/${COMPONENT} &>>${LOGFILE}
+# stat $?
 
 echo -n "Extracting $COMPONENT"
-cd /home/${FUSER} &>>${LOGFILE} # && unzip -o /tmp/${COMPONENT}.zip &>>${LOGFILE} && mv ${COMPONENT}-main ${COMPONENT} &>>${LOGFILE}
+cd /home/${FUSER} &>>${LOGFILE}  && unzip -o /tmp/${COMPONENT}.zip &>>${LOGFILE} && mv ${COMPONENT}-main ${COMPONENT} &>>${LOGFILE}
 stat $?
 
 }
