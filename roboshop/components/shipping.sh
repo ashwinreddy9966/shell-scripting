6e2a0bc912bd8c1238a7fbdd6cf19c518a562885
manu <manu@local.com>
2022-04-03 21:03:27 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -4,28 +4,21 @@ source components/common.sh
 COMPONENT=shipping
 
 MAVEN 
-# echo -n "Installing Maven : "
-# yum install maven -y &>> $LOGFILE
-# stat $?
 
-# echo -n "Adding $FUSER User : "
-# USER_SETUP
-# stat $?
+ echo -n "Downloading  $COMPONENT :"
+ curl -f -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOGFILE}
+ stat $?
 
-# echo -n "Downloading  $COMPONENT :"
-# curl -f -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOGFILE}
-# stat $?
+ echo -n "CleanUp Old Content : "
+ rm -rf /home/${FUSER}/${COMPONENT} &>>${LOGFILE}
+ stat $?
 
-# echo -n "CleanUp Old Content : "
-# rm -rf /home/${FUSER}/${COMPONENT} &>>${LOGFILE}
-# stat $?
+ echo -n "Extracting $COMPONENT"
+ cd /home/${FUSER} &>>${LOGFILE} && unzip -o /tmp/${COMPONENT}.zip &>>${LOGFILE} && mv ${COMPONENT}-main ${COMPONENT} &>>${LOGFILE}
+ stat $?
 
-# echo -n "Extracting $COMPONENT"
-# cd /home/${FUSER} &>>${LOGFILE} && unzip -o /tmp/${COMPONENT}.zip &>>${LOGFILE} && mv ${COMPONENT}-main ${COMPONENT} &>>${LOGFILE}
-# stat $?
-
-# echo -n "Building Artifact :"
-# cd shipping && mvn clean package &>>${LOGFILE} && mv target/$COMPONENT-1.0.jar $COMPONENT.jar &>>${LOGFILE}
+ echo -n "Building Artifact :"
+ cd shipping && mvn clean package &>>${LOGFILE} && mv target/$COMPONENT-1.0.jar $COMPONENT.jar &>>${LOGFILE}
 
 # echo -n "Configuring SystemD : "
 SVC_SETUP
