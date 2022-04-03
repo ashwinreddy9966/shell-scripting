f43914faf91c347c2d7c02fd673803dd656c32c7
manu <manu@local.com>
2022-04-03 16:55:30 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -28,11 +28,6 @@ USER_SETUP() {
 SVC_SETUP() {
   #1. Updating SystemD file with correct DNS Name
   echo -n "Updating the $COMPONENT DNS name : "
-#  sed -i -e 's/MONGO_DNSNAME/mongodb.robotlearning.internal/' \
-#         -e 's/REDIS_ENDPOINT/redis.robotlearning.internal/'  \
-#         -e 's/MONGO_ENDPOINT/mongodb.robotlearning.internal' \
-#             /home/${FUSER}/${COMPONENT}/systemd.service $>> ${LOGFILE}
-
   sed -i  -e 's/MONGO_DNSNAME/mongodb.robotlearning.internal/' \
           -e 's/REDIS_ENDPOINT/redis.robotlearning.internal/' \
           -e 's/MONGO_ENDPOINT/mongodb.robotlearning.internal/' \
