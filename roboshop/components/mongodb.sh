2b426021b2e3b12f62091b09713c5219fa1e1e85
manu <manu@local.com>
2022-04-03 08:56:24 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -3,6 +3,7 @@
 source components/common.sh
 
 echo -n "Configuring MongoDB Repo :"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 stat $?
 echo -n "Installing MongoDB : "
 yum install -y mongodb-org &>> $LOGFILE
@@ -12,17 +13,20 @@ echo -n "Starting Mongo : "
 systemctl start mongod  &>> $LOGFILE
 stat $?
 
-# 1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in config file
-  #
-  #Config file: `/etc/mongod.conf`
-# systemctl restart mongod
+# Update Listen IP address from 127.0.0.1 to 0.0.0.0 in config file
+echo -n "Updating the mongo proxt config file : "
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf
+stat $?
 
+echo -n "Restart Mongo : "
+systemctl restart mongod
 
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+echo -n "Downloading the schema : "
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+stat $?
 
-# cd /tmp
-# unzip mongodb.zip
-# cd mongodb-main
-# mongo < catalogue.js
-# mongo < users.js
+cd /tmp && unzip mongodb.zip  && cd mongodb-main
+echo -n "Injecting the schema : "
+mongo < catalogue.js && mongo < users.js
+stat $?
 
