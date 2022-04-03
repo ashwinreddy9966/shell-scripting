ccc201da4a68d126becf2948d2784d3aee04dc0b
manu <manu@local.com>
2022-04-03 13:27:31 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,6 +1,6 @@
 #validating whether the user been run is ROOT or not
-ID=$(id -u)
-if [ $ID -ne 0 ]; then
+ID="${APP_USER}" # &>>${LOG_FILE}
+if [ ${ID} -ne 0 ]; then
   echo -e "\e[33m You need to be a root user to execute this or execute this with sudo command \[0m"
   exit 1
 fi
