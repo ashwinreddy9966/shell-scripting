72ad3d858b874c36961572ce38aa2e0344765e1a
manu <manu@local.com>
2022-04-07 10:36:20 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -4,15 +4,15 @@ source components/common.sh
 COMPONENT=rabbitmq
 
 echo -n "Installing ERLang : "
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y $>> $LOGFILE
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> LOGFILE
 stat $?
 
 echo -n "configuring rabbitMQ Repo :"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash $>> $LOGFILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
 stat $?
 
 echo -n "Installing RabbitMQ :"
-yum install rabbitmq-server -y &>> $LOGFILE
+yum install rabbitmq-server -y &>> &LOGFILE
 stat $?
 
 echo -n "Starting RabbitMQ :"
@@ -21,5 +21,5 @@ systemctl start rabbitmq-server
 stat $?
 
 echo -n "Creating Application User: "
-rabbitmqctl add_user roboshop roboshop123 &>>${LOGFILE}
+rabbitmqctl add_user roboshop roboshop123 &>>&{LOGFILE}
 stat $?
