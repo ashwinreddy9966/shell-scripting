93f009726ae08763e91f672e74f17e9ae0f56b97
manu <manu@local.com>
2022-04-07 17:39:51 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -7,6 +7,12 @@ if [ -z "$1" ] ; then
 fi
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" |jq '.Images[].ImageId' | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=robot-allow-all |jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 echo $AMI_ID
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" |jq
+aws ec2 run-instances \
+        --image-id $AMI_ID \
+        --instance-type t3.micro \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" \
+        --security-group-ids $SGID \
+        | jq
