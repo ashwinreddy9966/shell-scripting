35ed4e79ec6ca68d395b7c181ae6cfe0bac62cea
manu <manu@local.com>
2022-04-07 18:08:39 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -10,6 +10,8 @@ ZONE_ID="Z0986768G0Z9NX43SZ0Z"
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" |jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=robot-allow-all |jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
+create_ec2() {
 PRIVATE_IP=$(aws ec2 run-instances \
                    --image-id $AMI_ID \
                    --instance-type t3.micro \
@@ -19,3 +21,13 @@ PRIVATE_IP=$(aws ec2 run-instances \
 
 sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+}
+
+if [ "$1" == "all" ]; then
+  for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis ; do
+    COMPONENT=$component
+    create_ec2
+  done
+else
+  create_ec2
+fi
