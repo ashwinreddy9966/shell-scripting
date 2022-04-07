5227beb0bc0f15977c03646248983a43bf1388f2
manu <manu@local.com>
2022-04-07 18:01:26 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -6,6 +6,7 @@ if [ -z "$1" ] ; then
   exit
 fi
 
+ZONE_ID="Z0986768G0Z9NX43SZ0Z"
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" |jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=robot-allow-all |jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
@@ -17,4 +18,4 @@ PRIVATE_IP=$(aws ec2 run-instances \
                    | jq '.Instances[].PrivateIpAddress' | sed 's/"/s/g')
 
 sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z0986768G0Z9NX43SZ0Z  --change-batch file://route53.json | jq
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
