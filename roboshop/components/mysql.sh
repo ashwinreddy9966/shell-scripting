a296d16183c8d08cc20e91da73bc4ec2b228fc92
manu <manu@local.com>
2022-04-07 12:15:32 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -19,15 +19,6 @@ stat $?
 #echo -n "Fetching the default root password : "
 #DEFAULT_ROOT_PASSWORD=$(sudo grep temp /var/log/mysqld.log | head -n 1 |  awk -F " " '{print $NF}')
 
-echo "show databases;" | mysql -uroot -pRoboShop@1 &>>${LOGFILE}
-if [ $? -ne 0 ]; then
-  echo -n "Changing the $COMPONENT root password"
-  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
-  DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-  mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOGFILE}
-  stat $?
-fi
-
 
 # Echo removing the password validate plugin :
 echo show plugins | mysql -uroot -pRoboShop@1 &>>${LOGFILE} | grep validate_password &>>${LOGFILE}
@@ -42,7 +33,7 @@ echo -n "Downloading the schema : "
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>${LOGFILE}
 stat $?
 
-echo -n "Extracting & Injecting Schema : "
+echo -n "Extracting Schema : "
 cd /tmp && unzip -o mysql.zip &>>${LOGFILE}
 stat $?
 
@@ -53,22 +44,3 @@ stat $?
 
 
 
-## mysql_secure_installation
-## mysql -uroot -pRoboShop@1
-#> uninstall plugin validate_password;
-#
-### **Setup Needed for Application.**
-#
-#As per the architecture diagram, MySQL is needed by
-#
-#- Shipping Service
-#
-#So we need to load that schema into the database, So those applications will detect them and run accordingly.
-#
-#To download schema, Use the following command
-#
-#```bash
-## curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-#```
-#
-#Load the schema for Services.
