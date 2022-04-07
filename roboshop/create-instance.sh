441d0bff9f8eaaa78ccfb2347f3cd1e40027a69f
manu <manu@local.com>
2022-04-07 17:58:10 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -7,12 +7,14 @@ if [ -z "$1" ] ; then
 fi
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" |jq '.Images[].ImageId' | sed -e 's/"//g')
-SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=robot-allow-all |jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 echo $AMI_ID
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=robot-allow-all |jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+PRIVATE_IP=$(aws ec2 run-instances \
+                   --image-id $AMI_ID \
+                   --instance-type t3.micro \
+                   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" \
+                   --security-group-ids $SGID \
+                   | jq '.Instances[].PrivateIpAddress' | sed 's/"/s/g')
 
-aws ec2 run-instances \
-        --image-id $AMI_ID \
-        --instance-type t3.micro \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" \
-        --security-group-ids $SGID \
-        | jq
+sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z0986768G0Z9NX43SZ0Z  --change-batch file://route53.json | jq
