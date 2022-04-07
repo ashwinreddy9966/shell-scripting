eb5d037ccc7375e35b3a208f917eed2748eb7231
manu <manu@local.com>
2022-04-07 11:20:17 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roboshop/components/frontend.sh

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -14,3 +14,11 @@ DOWNLOAD_AND_INSTALL
 echo -n "Installing Python Dependencies :"
 pip3 install -r requirements.txt &>> $LOGFILE
 stat $?
+
+echo "Update Application Config payment.ini"
+USER_ID=$(id -u roboshop)
+GROUP_ID=$(id -g roboshop)
+sed -i -e "/uid/ c uid = ${USER_ID}" -e "/gid/ c gid = ${GROUP_ID}" ${COMPONENT}.ini
+stat $?
+
+SVC_SETUP
